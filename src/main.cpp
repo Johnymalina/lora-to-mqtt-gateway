4daffe039ecf31a6b0709f2ca760eea04f1d4b52
Jakub <jakubcihanek@gmail.com>
2023-08-27 10:29:37 +0200

    debug

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+#if DEBUG == 1
+#define debug(x) Serial.print(x)
+#define debugln(x) Serial.println(x)
+#define debugBegin(x) Serial.begin(x)
+#else
+#define debug(x)
+#define debugln(x)
+#define debugBegin(x)
+#endif
+
 #include <Arduino.h>
 
 #include <WiFi.h>
