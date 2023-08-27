37fbcf67b54939d110002e39954ec17835315608
Jakub <jakubcihanek@gmail.com>
2023-08-27 10:28:32 +0200

    wifi init

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,25 @@ const char *password = "Mylifeforthehorde";
 
 void setup()
 {
+
+  WiFi.begin(ssid, password);
+  int counter = 0;
+  debug("Connecting WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    debug(".");
+    delay(200);
+    if (++counter > 100)
+    {
+      debugln("");
+      debugln("Cant connect to WiFi... Restarting");
+      delay(100);
+      ESP.restart();
+    }
+  }
+  debugln("");
+  debug("WiFi Connected to network: ");
+  debugln(WiFi.SSID());
 }
 
 void loop()
