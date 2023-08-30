8e1b0809fc2f552929e6579ee73340f62a225f0f
Jakub Čihánek <jakubcihanek@gmail.com>
2023-08-30 11:05:57 +0200

    debug msgs

--- src/main.cpp
+++ src/main.cpp
@@ -32,20 +32,20 @@ const int mqtt_port = 1883;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-char tempString[8];
+char tempString[256];
 
 void setup()
 {
   debugBegin(9600);
   // WIFI
   WiFi.begin(ssid, password);
-  int counter = 0;
+  int counterWifi = 0;
   debug("Connecting WiFi");
   while (WiFi.status() != WL_CONNECTED)
   {
     debug(".");
     delay(200);
-    if (++counter > 100)
+    if (++counterWifi > 100)
     {
       debugln("");
       debugln("Cant connect to WiFi... Restarting");
@@ -57,6 +57,7 @@ void setup()
   debug("WiFi Connected to network: ");
   debugln(WiFi.SSID());
   // LORA
+  debug("Connecting LoRa");
   LoRa.setPins(SS_PIN, RST_PIN, DIO0_PIN);
   if (!LoRa.begin(868E6))
   {
@@ -66,11 +67,26 @@ void setup()
   }
   else
   {
-    debugln("LoRa Runninng...");
+    debugln("LoRa Runninng");
   }
   // MQTT
   client.setServer(mqtt_broker, mqtt_port);
-  client.connect("lora_to_mqtt_gateway", mqtt_username, mqtt_password);
+  debug("MQTT Connecting");
+  int counterMqtt = 0;
+  while (!client.connect("lora_to_mqtt_gateway", mqtt_username, mqtt_password))
+  {
+    debug(".");
+    delay(200);
+    if (++counterMqtt > 100)
+    {
+      debugln("");
+      debugln("Cant connect to MQTT... Restarting");
+      delay(100);
+      ESP.restart();
+    }
+  }
+  debugln();
+  debugln("MQTT Connected");
 }
 
 void loop()
@@ -80,10 +96,11 @@ void loop()
     while (LoRa.available())
     {
       loraData = LoRa.readString();
-      debugln("LoRa Received data:");
-      debug(loraData);
+      debug("LoRa Received data:");
+      debugln(loraData);
     }
-    loraData.toCharArray(tempString, 8);
+    loraData.toCharArray(tempString, 256);
+    debugln(tempString);
     client.publish(topic, tempString);
   }
 }
