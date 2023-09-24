cf5de3b61ef2db516eedb20f1f3a523d70389900
Jakub <jakubcihanek@gmail.com>
2023-09-24 19:33:50 +0200

    mqtt file rm

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,57 @@ String loraData;
 
 #include <radio.h>
 
-#include <mqtt.h>
+#include <WiFiClient.h>
+#include <PubSubClient.h>
+const char *mqtt_broker = "10.10.42.4";
+const char *topic = "lora/mqtt";
+const char *mqtt_username = "mqtt-user";
+const char *mqtt_password = "HesloMqtt196455";
+const int mqtt_port = 1883;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void mqttConnect()
+{
+  client.setServer(mqtt_broker, mqtt_port);
+  debugln("MQTT Connecting");
+  int counterMqtt = 0;
+  client.connect("lora_to_mqtt_gateway", mqtt_username, mqtt_password);
+  while (!client.connected())
+  {
+    debug(".");
+    delay(200);
+    if (++counterMqtt > 100)
+    {
+      debugln("");
+      debugln("Cant connect to MQTT... Restarting");
+      delay(100);
+      ESP.restart();
+    }
+  }
+  debugln("MQTT Connected");
+  client.setKeepAlive(1000);
+}
+
+void mqttPublish()
+{
+  mqttConnected = client.connected();
+  if (msgToSend)
+  {
+    debugln("MQTT data avalaible to send");
+    int strLenght = loraData.length() + 1;
+    char mqttData[strLenght];
+    loraData.toCharArray(mqttData, strLenght);
+    while (!client.connect("lora_to_mqtt_gateway", mqtt_username, mqtt_password))
+      ;
+    debugln("MQTT reconnected");
+    while (!client.publish(topic, mqttData))
+    {
+      debugln("Sending MQTT data");
+    }
+    msgToSend = 0;
+  }
+}
 
 #include <website.h>
 
@@ -75,7 +125,7 @@ void setup()
   debugln("HTTP server started");
   server.on("/", []()
             { server.send(200, "text/html", Website::Update()); });
-  Mqtt::Connect();
+  mqttConnect();
 }
 
 void loop()
@@ -85,5 +135,5 @@ void loop()
 
   Radio::Receive();
 
-  Mqtt::Publish();
+  mqttPublish();
 }
