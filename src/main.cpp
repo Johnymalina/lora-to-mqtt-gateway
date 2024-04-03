74fd49cadb6a47e8a4902fab093a5205d02966a7
Jakub Cihanek <jakubcihanek@gmail.com>
2024-04-03 05:51:31 +0200

    added ota,web libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <DebugMonitor.h>
 
+#include <WiFiClient.h>
+#include <ETH.h>
+#define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
+#define ETH_PHY_POWER 12
+
 DebugMonitor debug;
 
 void setup()
