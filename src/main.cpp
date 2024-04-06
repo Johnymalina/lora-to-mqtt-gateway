987537132147aa9c5ee1a96d14abbbc3847a8d21
Jakub Cihanek <jakubcihanek@gmail.com>
2024-04-06 16:39:42 +0200

    commented new ETH #defines

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,14 @@ void loraInitialise()
   LoRa.receive();
 }
 
+// #define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
+// #define ETH_PHY_POWER -1
+// #define ETH_PHY_TYPE ETH_PHY_LAN8720
+// #define ETH_PHY_ADDR 0
+// #define ETH_PHY_MDC 23
+// #define ETH_PHY_MDIO 18
+// #define NRST 5
+
 #include <WiFiClient.h>
 #include <PubSubClient.h>
 #include <ETH.h>
