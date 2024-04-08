6ce79ff58f2137b269326876a7786b7eadab7087
Jakub Cihanek <jakubcihanek@gmail.com>
2024-04-08 15:34:10 +0200

    network debug msgs

--- src/NetworkConnection.cpp
+++ src/NetworkConnection.cpp
@@ -9,35 +9,49 @@ void WiFiEvent(WiFiEvent_t event)
     switch (event)
     {
     case ARDUINO_EVENT_ETH_START:
-        Serial.println("ETH Started");
-        // set eth hostname here
-        ETH.setHostname("esp32-ethernet");
+        ETH.setHostname("lora2mqtt-gateway");
+#ifdef DEBUG
+        debug("INFO: ");
+        debugln("Ethernet Network Started");
+#endif
         break;
     case ARDUINO_EVENT_ETH_CONNECTED:
-        Serial.println("ETH Connected");
+#ifdef DEBUG
+        debug("INFO: ");
+        debugln("Ethernet Network Connected");
+#endif
         break;
+
     case ARDUINO_EVENT_ETH_GOT_IP:
-        Serial.print("ETH MAC: ");
-        Serial.print(ETH.macAddress());
-        Serial.print(", IPv4: ");
-        Serial.print(ETH.localIP());
-        if (ETH.fullDuplex())
-        {
-            Serial.print(", FULL_DUPLEX");
-        }
-        Serial.print(", ");
-        Serial.print(ETH.linkSpeed());
-        Serial.println("Mbps");
+#ifdef DEBUG
+
+        debug("INFO: ");
+        debug("Ethernet IPv4: ");
+        debug(ETH.localIP());
+        debug(", Hostname: ");
+        debug(ETH.getHostname());
+        debug(", MAC: ");
+        debug(ETH.macAddress());
+        debug(", Speed: ");
+        debug(ETH.linkSpeed());
+        debugln("Mbps");
+#endif
         eth_connected = true;
         break;
     case ARDUINO_EVENT_ETH_DISCONNECTED:
-        Serial.println("ETH Disconnected");
+
+#ifdef DEBUG
+        debugln("ETH Disconnected");
+#endif
         eth_connected = false;
         break;
     case ARDUINO_EVENT_ETH_STOP:
-        Serial.println("ETH Stopped");
+#ifdef DEBUG
+        debugln("ETH Stopped");
+#endif
         eth_connected = false;
         break;
+
     default:
         break;
     }
@@ -56,5 +70,12 @@ void NetworkConnection::begin()
 
 void NetworkConnection::setCallback()
 {
+#ifdef DEBUG
+
+    debug("INFO: ");
+    debugln("Network Callback Setup");
+
+#endif
+
     WiFi.onEvent(WiFiEvent);
 }
