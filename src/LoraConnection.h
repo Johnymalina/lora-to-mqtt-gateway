b6361c5302cfebbee0df16fe040e8f8d85b3d141
Jakub Cihanek <jakubcihanek@gmail.com>
2024-04-14 22:05:34 +0200

    rearange

--- src/LoraConnection.h
+++ src/LoraConnection.h
@@ -1,16 +1,16 @@
 #ifndef LORA_CONNECTION_H
 #define LORA_CONNECTION_H
 
+#include <Arduino.h>
+#include <DebugMonitor.h>
+#include <LoRa.h>
+
 #define SCK 14
 #define MISO 32
 #define MOSI 33
 #define SS 16
 #define RST 4
 #define DIO0 2
-
-#include <Arduino.h>
-#include <DebugMonitor.h>
-#include <LoRa.h>
 
 class LoraConnection
 {
